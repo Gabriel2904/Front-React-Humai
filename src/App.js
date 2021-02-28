faf4d296c3edca53e0ddd984f9877369b872be66
Gabriel2904 <gabriel2904@hotmail.com>
2021-02-28 04:55:36 -0300

    Front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Humai from "./1_FINO.png";
+//import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header/";
+import Footer from "./Components/Footer/";
+import { Col, Row } from "react-bootstrap";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row>
+        <Col md={12}>
+          <Header />
+          <header className="App-header">
+            <img src={Humai} className="App-logo" alt="Humai" />
+          </header>
+        </Col>
+        <Footer />
+      </Row>
+    </>
   );
 }
 
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="text">Creando Footer</p>
+    </div>
+  );
+};
+
+export default Footer;
